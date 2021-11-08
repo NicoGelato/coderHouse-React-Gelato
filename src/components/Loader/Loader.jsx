2deabfd6dedb80c8cf1193c4daad8e1a6ebfdeab
NicoGelato <86999857+NicoGelato@users.noreply.github.com>
2021-11-07 22:46:08 -0300

    Proyecto Final Auxilio

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,33 +1,39 @@
 import { useEffect, useState } from "react";
 
 const Loader = () => {
-/*
-  const [state, setState] = useState(false)
+  const [tardoMucho, setTardoMucho] = useState(false);
 
   useEffect(() => {
-    setTime
+    const esperar = setTimeout(() => {
+      setTardoMucho(true);
+    }, 30000);
     return () => {
-      setTime.cancel()
-    }
-  }, [])
-*/
-
+      clearTimeout(esperar);
+    };
+  }, []);
 
   return (
-
-
-      
-      
     <div className="bg-dark p-2 pt-3 opacity-75">
-      <h1 className="text-center text-white h3">☠️ Cargando ... ☠️</h1>
-      <div className="d-flex justify-content-center m-5">
-        <div
-          className="spinner-border spinner-grow-sm text-light mx-1"
-          role="status"
-        >
-          <span className="visually-hidden">Cargando...</span>
-        </div>
-      </div>
+      {tardoMucho ? (
+        <>
+          <h1 className="text-center text-white h3">
+            ☠️ No se pudieron cargar los productos, por favor intente más tarde
+            ☠️
+          </h1>
+        </>
+      ) : (
+        <>
+          <h1 className="text-center text-white h3">☠️ Cargando ... ☠️</h1>
+          <div className="d-flex justify-content-center m-5">
+            <div
+              className="spinner-border text-light mx-1"
+              role="status"
+            >
+              <span className="visually-hidden">Cargando...</span>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
