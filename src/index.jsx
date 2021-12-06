dffa696c7dad82dee1d8d1f28b4ec1e4a2ccfe74
NicoGelato <86999857+NicoGelato@users.noreply.github.com>
2021-12-06 01:04:01 -0300

    Agregue el css de las notificaciones ReactToastify.css a src

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./scss/app.scss";
-import "react-notifications/lib/notifications.css";
-import "node_modules/react-toastify/dist/ReactToastify.css";
+import "./scss/css/ReactToastify.css";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
